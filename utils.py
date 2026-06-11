73a19060d0e45d46b23b8d43f51fd4baca242454
Jzee <60868715+Jzee-studio@users.noreply.github.com>
2026-06-11 14:12:45 +0800

    Switch to HFDR training; update env & utils
    Update configs to use HFDR: change test Prefix to 'WRN34_10_F', train Prefix to 'WRN34_10_F_HFDR' and set Train_Method to 'HFDR'. Update environment.yaml to add the 'nvidia' channel and pip, fix torchaudio spacing, correct 'tdqm' -> 'tqdm', and add mkl==2024.0. Comment out loading of ./data/corruptions.txt in utils.py to avoid a hard dependency on that file.

--- utils.py
+++ utils.py
@@ -32,7 +32,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 def load_txt(path :str) -> list:
     return [line.rstrip('\n') for line in open(path)]
 
-corruptions = load_txt('./data/corruptions.txt')
+# corruptions = load_txt('./data/corruptions.txt')
 
 class CIFAR10C(datasets.VisionDataset):
     def __init__(self, root :str, name :str, 
